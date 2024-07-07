857fe2bd559497406935ac26663462d7646b2771
Sputnik-v <igortronin2705@gmail.com>
2024-07-07 21:48:16 +0400

    avatars

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreAuthRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
@@ -23,9 +24,17 @@ class LoginController extends Controller
 
         if(Auth::attempt($data, $remember))
         {
+            $collectPhoto = $request->user()->images()->where('active_image', true)->get();
+            $srcImage = $collectPhoto->toArray();
+            $arraySrc = Arr::collapse($srcImage);
+            if(!$arraySrc)
+            {
+                $arraySrc = '';
+            }
+
             $request->session()->regenerate();
 
-            return redirect()->route('dashboard', ['user' => $request->user()]);
+            return redirect()->route('dashboard', ['user' => $request->user(), 'srcImage' => $arraySrc]);
         }
 
         return back()->withErrors([
